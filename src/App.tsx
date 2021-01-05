0ef3357107a27915cf7ccb94bd3575f6bcfaef65
Finn Pauls <mbp@pauls.dev>
2021-01-05 18:32:04 +0100

    Show browser position radius

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,49 @@
-import {tileLayer, map} from 'leaflet'
-import React, {useEffect} from 'react'
+import L from 'leaflet'
+import {useEffect, useState} from 'react'
+
+const map = L.map('mapid')
+const initialZoom = 11
+const circle = createCircle()
 
 export default function App() {
+  const [position, setPosition] = useState<[number, number]>([51.505, -0.09])
   useLeaflet()
-  return <div id="mapid"></div>
+  useBrowserLocation()
+  useUpdatePosition()
+
+  return null
+
+  function useLeaflet() {
+    useEffect(() => {
+      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map)
+    }, [])
+  }
+
+  function useBrowserLocation() {
+    useEffect(() => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setPosition([position.coords.latitude, position.coords.longitude])
+      })
+    }, [])
+  }
+
+  function useUpdatePosition() {
+    useEffect(() => {
+      map.setView(position, initialZoom)
+      circle?.setLatLng(position)
+    }, [position])
+  }
 }
 
-function useLeaflet() {
-  useEffect(() => {
-    const mymap = map('mapid').setView([51.505, -0.09], 13)
-
-    tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(mymap)
-  }, [])
+function createCircle(): L.Circle {
+  const color = '#d82a36'
+  return L.circle([51.508, -0.11], {
+    color,
+    fillColor: color,
+    fillOpacity: 0.2,
+    radius: 15000
+  }).addTo(map)
 }
